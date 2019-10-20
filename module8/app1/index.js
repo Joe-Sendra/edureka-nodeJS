547417569517b9afa05255c07199581874d7d78a
Joe-Sendra <joe.sendra@gmail.com>
2019-10-20 12:26:48 -0500

    Initial commit

---
+++ module8/app1/index.js
@@ -0,0 +1,15 @@
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket)=>{
+    console.log('*** a user has connected ***')
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
